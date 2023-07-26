224639d90b5c081e8f623bbcc1813576bf415f20
TimDiam0nd <54599998+TimDiam0nd@users.noreply.github.com>
2023-07-26 22:06:42 +0100

    Update genelex.py

--- alyahmor/genelex.py
+++ alyahmor/genelex.py
@@ -32,9 +32,9 @@ import stopword_affixer
 class genelex:
     
     def __init__(self,):
-        self.verb_vocalizer = verb_affixer.verb_affixer()        
-        self.noun_vocalizer = noun_affixer.noun_affixer()        
-        self.stopword_vocalizer = stopword_affixer.stopword_affixer()
+        self.verb_vocalizer = verb_affixer()        
+        self.noun_vocalizer = noun_affixer()        
+        self.stopword_vocalizer = stopword_affixer()
         pass
     def generate_by_affixes(self, word, word_type="noun", affixes =[], vocalized=True,):
         """
